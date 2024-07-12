d51fabff2c9b0ab34b88f489ca2a032260470e86
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-07-12 23:39:52 +0530

    update authlogin page

--- src/components/auth/AuthSideImg.tsx
+++ src/components/auth/AuthSideImg.tsx
@@ -7,18 +7,6 @@ const AuthSideImg = () => {
         </h1>
  
 
-        {/* <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
-          One Website <br />
-          <span className="block text-lg md:text-5xl mt-6">
-            Many Roles
-          </span>
-        </h1>
-          <p className="text-neutral-500 max-w-md  mx-auto my-2 text-2xl text-center relative z-10">
-      
-          </p>
-        </div> */}
-
 
 <div className="max-w-2xl mx-auto p-4">
 <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
