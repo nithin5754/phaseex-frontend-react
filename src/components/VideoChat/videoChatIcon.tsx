470e939a2263022681cc7798d8f2fb47688c4b89
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-07-23 13:40:09 +0530

    rectify

--- src/components/VideoChat/videoChatIcon.tsx
+++ src/components/VideoChat/videoChatIcon.tsx
@@ -1,8 +1,8 @@
-import { LottieAnimation } from "../lootie/Lootie";
 
 
-import ChatIcon from "../../../public/json/chat-icon.json";
-import { Button } from "../ui/button";
+
+
+
 
 import { CJModal } from "../modal/Create-Join-modal";
 
@@ -13,9 +13,9 @@ const VideoChatIcon = () => {
     <div className="flex flex-row items-center justify-center gap-4">
 
 <CJModal/>
-      <Button className="h-[100px]">
+      {/* <Button className="h-[100px]">
         <LottieAnimation animationData={ChatIcon} height={100} width={100} />
-      </Button>
+      </Button> */}
     </div>
   );
 };
