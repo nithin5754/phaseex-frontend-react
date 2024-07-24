1e1cf26afa1796c83145ca28bcf2eaaa5b250cac
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-07-24 22:40:51 +0530

    update

--- src/features/main-sections/HomePage.tsx
+++ src/features/main-sections/HomePage.tsx
@@ -5,7 +5,7 @@ import EmptyRecent from "../../../public/json/empty-work-2.json";
 const HomePage = () => {
   return (
     <>
-      <div className="container min-h-[600px] mx-auto p-4 flex  dark:bg-background dark:text-white dark:gap-4 ">
+      <div className="mx-auto p-4 flex  dark:bg-background dark:text-white dark:gap-4 ">
         <div className="min-h-[500px] flex flex-wrap gap-4 w-full dark:bg-card dark:text-white  dark:border dark:border-border dark:rounded-sm">
           <div className="w-full max-w-lg p-4 rounded-lg shadow-md">
             <div className="flex justify-between items-center mb-4">
