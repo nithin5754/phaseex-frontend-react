20788934569cc4967ba675590a85f1f2a6687bb6
Nithin Joji0756 <nithinjoji0756@gmail.com>
2024-08-05 19:48:29 +0530

    upsate

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,6 +1,8 @@
 import { BackgroundBeams } from "@/components/aceternityuI/Background/background-beams";
 import { AuthLogin, AuthSideImg } from "../../components/auth/index";
 import GoogleAuth from "@/components/auth/google/GoogleAuth";
+import { Copy } from "lucide-react";
+import UseHandleCopyLink from "@/hooks/useLinkCopy";
 
 const Login = () => {
   return (
@@ -13,6 +15,27 @@ const Login = () => {
         <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
           Login to phaseex app that replace them all other apps
         </p>
+
+        <div className="my-4 flex flex-col gap-2">
+          {" "}
+     <div className="flex ">
+     <h1 className="text-bold"> <span className="text-sm">Email:</span> phaseex@gmail.com</h1>
+          <Copy
+            size={14}
+            className="cursor-pointer "
+            onClick={() => UseHandleCopyLink("phaseex@gmail.com")}
+          />
+     </div>
+         <div className="flex">
+         <h2 className="text-bold"> <span className="text-sm">Password:</span> password:Nivin@1234</h2>
+         <Copy
+            size={14}
+            className="cursor-pointer "
+            onClick={() => UseHandleCopyLink("Nivin@1234")}
+          />
+         </div>
+      
+        </div>
 
         <AuthLogin />
         <h3 className="text-center font-sfpro  text-slate-600 my-4">
